81d6bfd0e634908f27d71684ea0f9750735a6452
A00302457 <144638631+A00302457@users.noreply.github.com>
2024-04-04 21:00:14 +0000

    Factory class made but having some errors.

--- WeatherMonitoringSystem/WeatherStationFactory.cs
+++ WeatherMonitoringSystem/WeatherStationFactory.cs
@@ -2,6 +2,24 @@ namespace WeatherMonitoringSystem
 {
     public class WeatherStationFactory
     {
-       public static 
+       public IDisplayInterface CreateWeatherStation(string type, WeatherDataSingleton<T> weatherDataSingleton)
+        {
+            IDisplayInterface? weatherStation = null;
+            switch (type)
+            {
+                case "CurrentConditionsDisplay":
+                    weatherStation = new CurrentConditionsDisplay(weatherDataSingleton);
+                    break;
+                case "StatisticsDisplay":
+                    weatherStation = new StatisticsDisplay(weatherDataSingleton);
+                    break;
+                case "ForecastDisplay":
+                    weatherStation = new ForecastDisplay(weatherDataSingleton);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid weather station type");
+            }
+            return weatherStation;
+        }
     }
 }
